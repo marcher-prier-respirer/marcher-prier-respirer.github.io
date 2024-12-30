3d241f4d941e49fa1659c7da27a1593b5eddfe07
marcher-prier-respirer <rj.vdp@pm.me>
2024-12-30 22:01:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let lastScrollPosition = 0;
 const nav = document.querySelector('nav');
+const header = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
     const currentScrollPosition = window.pageYOffset;
 
-    if (currentScrollPosition > lastScrollPosition && currentScrollPosition > 172) {
-        // Scrolling down - hide the menu
-        // nav.style.position = 'fixed';
+    if (currentScrollPosition > header.offsetHeight) {
+        // Scroll past the header: make the menu stick to the top & hide it
+        nav.style.position = 'fixed';
         nav.style.top = '-50px'; // Keeps a small visible edge
     } else {
-        // Scrolling up - show the menu
-        nav.style.position = 'fixed';
-        nav.style.top = '0';
+        // Scroll within the header: keep the menu below the header
+        nav.style.position = 'relative';
+        nav.style.top = 'initial'; // Reset top when position is relative
     }
 
     lastScrollPosition = currentScrollPosition;
